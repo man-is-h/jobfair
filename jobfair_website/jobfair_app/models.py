f11b16b7e3f68b761504f37d400a5233f9f73b4f
mranish592 <mranish592@gmail.com>
2021-04-26 03:12:57 +0530

    added all models and fixed registration.html

--- jobfair_website/jobfair_app/models.py
+++ jobfair_website/jobfair_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length = 256)
@@ -13,6 +14,19 @@ class Project(models.Model):
     stipend = models.IntegerField(
         default = 0,
      )
+
+    upvote = models.IntegerField(
+        default = 0,
+    )
+
+    downvote = models.IntegerField(
+        default = 0,
+    )
+
+    skills = models.TextField(
+    default = '',
+    max_length = 500
+    )
 
     def __str__(self):
         return self.name
@@ -35,8 +49,24 @@ class Freelancer(models.Model):
 class UserProfileInfo(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE,)
+
     #aditional
     rating = models.IntegerField(default = 0,)
+    description = models.CharField(
+               default = '',
+               max_length = 200
+               )
+
+    class Role(models.TextChoices):
+        FREELANCER = 'freelancer', _('freelancer')
+        EMPLOYER = 'employer', _('employer')
+
+    role=models.CharField(
+        max_length=100,
+        choices=Role.choices,
+        default=Role.FREELANCER,
+    )
+    project = models.ForeignKey(Project, related_name = 'users', on_delete=models.CASCADE,default = 1)
 
     def __str__(self):
         return self.user.username
